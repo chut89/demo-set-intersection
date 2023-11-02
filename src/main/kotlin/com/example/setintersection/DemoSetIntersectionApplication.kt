45dc0476eca6af8ee1096aa1f902d4c0e24986cc
chut89 <chutienha_1989@yahoo.com>
2023-11-02 06:52:00 +0100

    add maven temp files to gitignore, try to customize SpringApplication.run, UPDATE KOTLIN VERSION TO 1.9.20, fix mvn warnings, remove unused dependencies

--- src/main/kotlin/com/example/setintersection/DemoSetIntersectionApplication.kt
+++ src/main/kotlin/com/example/setintersection/DemoSetIntersectionApplication.kt
@@ -2,10 +2,11 @@ package com.example.setintersection
 
 import org.springframework.boot.autoconfigure.SpringBootApplication	
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.Banner
 
 @SpringBootApplication
 class DemoSetIntersectionApplication
 
 fun main(args: Array<String>) {
-    SpringApplication.run(DemoSetIntersectionApplication::class.java, *args)
+    SpringApplication(DemoSetIntersectionApplication::class.java).apply{ setBannerMode(Banner.Mode.CONSOLE) }.run(*args)
 }
