921ddc78c6223e61a03c0580207e216b554fe6d9
chut89 <chutienha_1989@yahoo.com>
2023-11-20 18:01:59 +0100

    fix compilation warning

--- src/main/kotlin/com/example/setintersection/SetIntersectionRequestHandler.kt
+++ src/main/kotlin/com/example/setintersection/SetIntersectionRequestHandler.kt
@@ -16,6 +16,7 @@ import org.springframework.web.reactive.function.server.buildAndAwait
 @Component
 class SetIntersectionRequestHandler(private val setIntersectionService: SetIntersectionService) {
 
+    @Suppress("UNCHECKED_CAST")
     suspend fun setIntersection(request: ServerRequest): ServerResponse {
         val (firstCollection, secondCollection) = 
             request.body(BodyExtractors.toMono(Pair::class.java)).awaitSingle() 
@@ -35,8 +36,8 @@ class SetIntersectionRequestHandler(private val setIntersectionService: SetInter
         val secondCollection: List<Int>? = request.queryParams().get("secondCollection")?.stream()?.map{ it.toInt(10) }?.toList()
         We enforce the queryParams to be of format firstCollection=1,2&secondCollection=2,3
         */
-        val firstCollection = request.queryParam("firstCollection")?.get()?.split(",")?.stream()?.map{ it.toInt(10) }?.toList()
-        val secondCollection = request.queryParam("secondCollection")?.get()?.split(",")?.stream()?.map{ it.toInt(10) }?.toList()
+        val firstCollection = request.queryParam("firstCollection").orElse(null)?.split(",")?.stream()?.map{ it.toInt(10) }?.toList()
+        val secondCollection = request.queryParam("secondCollection").orElse(null)?.split(",")?.stream()?.map{ it.toInt(10) }?.toList()
         
         if (firstCollection == null || secondCollection == null) {
             return ServerResponse.badRequest().buildAndAwait()
@@ -49,7 +50,7 @@ class SetIntersectionRequestHandler(private val setIntersectionService: SetInter
     }
     
     suspend fun getRandomIntegerList(request: ServerRequest): ServerResponse {
-        val size: Long? = request.queryParam("size")?.get()?.toLong()
+        val size: Long? = request.queryParam("size").orElse(null)?.toLong()
         
         if (size == null || size < 0) {
             return ServerResponse.badRequest().buildAndAwait()        
