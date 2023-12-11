abe620d6e0a6dfd564331150bcebf9beb4426105
chut89 <chutienha_1989@yahoo.com>
2023-12-11 13:55:01 +0100

    unit test: remove unused import, improve matcher conditions in tests

--- src/test/kotlin/com/example/setintersection/RouterTest.kt
+++ src/test/kotlin/com/example/setintersection/RouterTest.kt
@@ -14,6 +14,7 @@ import org.springframework.web.reactive.function.server.RouterFunction
 import org.springframework.web.reactive.function.server.ServerResponse
 
 import org.mockito.kotlin.any
+import org.mockito.kotlin.eq
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
@@ -34,8 +35,7 @@ import com.example.setintersection.SetIntersectionRouter
 import com.example.config.SecurityConfig
 
 @ExtendWith(SpringExtension::class)
-@ContextConfiguration(classes = [ SetIntersectionRequestHandler::class, SetIntersectionRouter::class, TestOverridenConfiguration::class,
-    SecurityConfig::class // without this SecurityFilterChain cannot be loaded and therefore every page is prohibited!!!
+@ContextConfiguration(classes = [ SetIntersectionRequestHandler::class, SetIntersectionRouter::class, TestOverridenConfiguration::class
 ])
 @EnableAutoConfiguration
 class RouterTest {
@@ -62,13 +62,13 @@ class RouterTest {
 	
 	@Test
 	fun testSetIntersectionSimple() {
-	    whenever(serviceMock.computeIntersection(any(), any())).thenReturn(Pair(setOf(3, 4), "123us"))
+	    whenever(serviceMock.computeIntersection(eq(listOf(9999, 9998, 9997, 9996)), eq(listOf(9995, 9994)))).thenReturn(Pair(setOf(3, 4), "123us"))
 	
 	    webTestClient
 	            .get().uri{ builder -> builder
                 .path("/api/setintersection/simple")
-                    .queryParam("firstCollection", listOf(9999,9999,9999,9999).toStringAsQueryParam())
-                        .queryParam("secondCollection", listOf(9999, 9999).toStringAsQueryParam()).build() }
+                    .queryParam("firstCollection", listOf(9999, 9998, 9997, 9996).toStringAsQueryParam())
+                        .queryParam("secondCollection", listOf(9995, 9994).toStringAsQueryParam()).build() }
           .exchange()
           .expectStatus().isOk()
           .expectBody()
@@ -80,21 +80,21 @@ class RouterTest {
 	
 	@Test
 	fun testSetIntersectionComplex() {
-	    whenever(serviceMock.computeIntersection(any(), any())).thenReturn(Pair(setOf(3, 4), "123us"))
+	    whenever(serviceMock.computeIntersection(eq(listOf(9999, 8888, 7777, 6666)), eq(listOf(1111, 2222)))).thenReturn(Pair(setOf(5, 6), "987us"))
 	
 	    webTestClient
 	            .post().uri("/api/setintersection/complex")
           .accept(MediaType.APPLICATION_JSON)
-          .bodyValue(Pair(listOf(9999, 9999, 9999, 9999), listOf(9999, 9999)))
+          .bodyValue(Pair(listOf(9999, 8888, 7777, 6666), listOf(1111, 2222)))
           .exchange()
           .expectStatus().isOk()
           .expectBody()
                        .jsonPath("$['first'].length()").isEqualTo(2)
-                       .jsonPath("$['first'][0]").isEqualTo(3)
-                       .jsonPath("$['first'][1]").isEqualTo(4)
-                       .jsonPath("$['second']").isEqualTo("123us")	
+                       .jsonPath("$['first'][0]").isEqualTo(5)
+                       .jsonPath("$['first'][1]").isEqualTo(6)
+                       .jsonPath("$['second']").isEqualTo("987us")	
 	}
-	
+
     private fun defaultBase64EncodedCredential(): String = Base64.getEncoder().encodeToString("user:password".toByteArray(StandardCharsets.UTF_8))
 
     private fun List<Int>.toStringAsQueryParam(): String {
