78895468a684e571d0c1c550bf9b0b3572fc18fd
chut89 <chutienha_1989@yahoo.com>
2023-12-12 21:53:16 +0100

    integration test: comment reason why @ConstructorBinding does not work in test environment

--- src/main/kotlin/com/example/config/SpringdocProperties.kt
+++ src/main/kotlin/com/example/config/SpringdocProperties.kt
@@ -3,7 +3,8 @@ package com.example.config
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties("springdoc")
-// declare var instead of val inside constructor expression, otherwise Springboot may have problem creating String bean for 'version'
+// declare var instead of val inside constructor expression, otherwise Springboot will have problem creating String bean for 'version' in test environment
+// even when @ConstructorBinding works on the fly since Springboot 3
 data class SpringdocProperties(var version: String = "", var swaggerUi: SwaggerUi? = null) {
     data class SwaggerUi(var useRootPath: Boolean? = true)
 }
